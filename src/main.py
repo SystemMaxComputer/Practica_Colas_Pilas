4e496f49e5b7f5e3aae4ea64e32c2fecef2f6534
CRISTOPHER <geronimosanchezromero@gmail.com>
2025-03-25 16:12:19 -0500

    intento 1

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from src.simulador import iniciar_simulacion
+
+if __name__ == "__main__":
+    try:
+        iniciar_simulacion()
+    except KeyboardInterrupt:
+        print("\nSimulación finalizada por el usuario.")
